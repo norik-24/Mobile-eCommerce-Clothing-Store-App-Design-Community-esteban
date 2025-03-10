f738d36d994b7d02c4ea49ebaa6f6e716815a1c4
norik-24 <187893295+norik-24@users.noreply.github.com>
2025-03-10 12:00:40 -0500

    feat: :art: se reestructuro todo el codigo de imagenes

--- js/home.js
+++ js/home.js
@@ -10,3 +10,22 @@ buttons.forEach(button => {
     });
 });
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const heartButtons = document.querySelectorAll(".heart-btn");
+
+    heartButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const heartIcon = this.querySelector("img");
+
+            if (this.classList.contains("liked")) {
+                heartIcon.src = "../storage/img/heart.png"; // Imagen de corazón vacío
+                this.classList.remove("liked");
+            } else {
+                heartIcon.src = "../storage/img/heart-filled.png"; // Imagen de corazón lleno
+                this.classList.add("liked");
+            }
+        });
+    });
+});
+
